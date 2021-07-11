d3d4fc391ca26d8509814052ca01714cb15ccf29
leetaehee <lastride25@naver.com>
2021-07-11 12:33:10 +0900

    4장 변수

---
+++ ch4/ex4.1/ex4.1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10 // a 변수 선언 
+	var msg string = "Hello Variable" // msg 변수 선ㅇ넌
+
+	a = 20 // a값 변경
+	msg = "Good Morning" // msg 값 변경
+	fmt.Println(msg, a) // msg 와 a	값 출력
+}
