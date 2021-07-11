d73a19501824d0d297f3eb672befed79afa6cee8
leetaehee <lastride25@naver.com>
2021-07-11 21:53:50 +0900

    6장 연산자

---
+++ ch6/ex6.3/ex6.3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int8 = 4 // 8비트 정수 
+	var y int8 = 64 // 8비트 정수 
+
+	fmt.Printf("x:%08b x<<2: %08b x<<2: %d\n", x, x << 2, x << 2) // 왼쪽시프트
+	fmt.Printf("x:%08b x<<2: %08b x<<2: %d\n", y, y << 2, y << 2) // 왼쪽시프트
+}
