45185db4da7453903dcd51b08c31bf9a1b44c29b
leetaehee <lastride25@naver.com>
2021-07-20 22:38:12 +0900

    9장 실습 완료

--- ch8/ex8.4/ex8.4.go
+++ ch8/ex8.4/ex8.4.go
@@ -6,9 +6,9 @@ const PI = 3.14              // 타입 없는 상수
 const FloatPI float64 = 3.14 // float64 타입 상수
 
 func main() {
-	var a int = PI * 100      // 오류가 발생하지 않습니다.
-	var b int = FloatPI * 100 // 타입 오류 발생
+	var a int = PI * 100 // 오류가 발생하지 않습니다.
+	//var b int = FloatPI * 100 // 타입 오류 발생
 
 	fmt.Println(a)
-	fmt.Println(b)
+	//fmt.Println(b)
 }
