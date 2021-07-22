402f90a30d84dc32f7f978ad26ac452f95dcad8d
leetaehee <lastride25@naver.com>
2021-07-22 23:27:51 +0900

    12장 배열

---
+++ ch12/ex12.2/ex12.2.go
@@ -0,0 +1,12 @@
+package main
+
+const Y int = 3 // 상수
+
+func main() {
+	//x := 5
+	//a := [x]int{1, 2, 3, 4, 5} // 변수
+
+	//b := [Y]int{1, 2, 3}
+
+	//var c [10]int
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/leetaehee/goproject.git
-
-go 1.16
