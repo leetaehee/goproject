2944fae7d69491150a8d86ea198f590af415c2e5
leetaehee <lastride25@naver.com>
2021-10-04 20:10:34 +0900

    30장 실습 완료

--- ch29/ex29.6/go.mod
+++ ch29/ex29.6/go.mod
@@ -2,4 +2,7 @@ module ex29.6
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ ch30/ex30.1/ex30_1_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/students", nil)
+	// /students 경로 테스트
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	var list []Student
+	err := json.NewDecoder(res.Body).Decode(&list) // 결과 변환
+	assert.Nil(err)                                // 결과 확인
+	assert.Equal(2, len(list))
+	assert.Equal("aaa", list[0].Name)
+	assert.Equal("bbb", list[1].Name)
+}
+
+func TestJsonHandler2(t *testing.T) {
+	assert := assert.New(t)
+
+	var student Student
+	mux := MakeWebHandler()
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/students/1", nil) // id 1 학생
+
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+	err := json.NewDecoder(res.Body).Decode(&student)
+	assert.Nil(err)
+	assert.Equal("aaa", student.Name)
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/students/2", nil) // id 2 학생
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+	err = json.NewDecoder(res.Body).Decode(&student)
+	assert.Nil(err)
+	assert.Equal("bbb", student.Name)
+}
+
+func TestJsonHandler3(t *testing.T) {
+	assert := assert.New(t)
+
+	var student Student
+	mux := MakeWebHandler()
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/students", strings.NewReader(`{"Id":0, "Name":"ccc", "Age":15,"Score":78}`)) // 새로운 학생 데이터
+
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusCreated, res.Code) // 응답 코드 검사
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/students/3", nil)
+	// 추가된 학생 데이터
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+	err := json.NewDecoder(res.Body).Decode(&student)
+	assert.Nil(err)
+	assert.Equal("ccc", student.Name)
+}
+
+func TestJsonHandler4(t *testing.T) {
+	assert := assert.New(t)
+
+	mux := MakeWebHandler()
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("DELETE", "/students/1", nil)
+	// DELETE 요청
+
+	mux.ServeHTTP(res, req)
+	assert.Equal(http.StatusOK, res.Code) // 응답 코드 검사
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/students", nil) // /students 경로
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	var list []Student
+	err := json.NewDecoder(res.Body).Decode(&list)
+	assert.Nil(err)
+	assert.Equal(1, len(list)) // 결과 확인
+	assert.Equal("bbb", list[0].Name)
+}
